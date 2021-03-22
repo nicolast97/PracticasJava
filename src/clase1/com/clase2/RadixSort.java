0e7d789b7582a6a2f3d5e263d25af6946b450429
Nicolas Taborda <nicolas.taborda@mercadolibre.com>
2021-03-22 18:56:30 -0300

    Agregado lpad

---
+++ src/clase1/com/clase2/RadixSort.java
@@ -0,0 +1,11 @@
+package com.clase2;
+
+public class RadixSort {
+
+    public static void main(String[] args) {
+        StringUtil stringUtil = new StringUtil();
+        String s = "prueba";
+        System.out.println(stringUtil.lpad(s,15,'x'));
+    }
+
+}
